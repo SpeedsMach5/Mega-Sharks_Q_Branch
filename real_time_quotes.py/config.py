bc489802cefda73c3cb7bea193c795d428dc1404
Quentin <quentin.s.reynolds@gmail.com>
2022-01-05 15:06:25 -0600

    added real_time_quotes.py

---
+++ real_time_quotes.py/config.py
@@ -0,0 +1,9 @@
+pkey= "XOVey1bBwhsgZ6uEtKJnxLCxW8JghTJSJqhfM8mR"
+skey= "PK3W3ZLACWGJUSQ8UI6R" 
+
+Headers = {
+    "APCA-API-KEY-ID": pkey,
+    "APCA-API-SECRET-KEY": skey
+}
+
+BARS_URL = 'https:// data.alpaca.markets/v2/stocks/{symbol}/bars'
