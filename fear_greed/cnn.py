6448351907290a5b91d3775f0ce32f7cfe029826
EGeorge2021r <egeorge2013khs@gmail.com>
2022-01-08 23:31:31 -0600

    Fear and greed module codeing

---
+++ fear_greed/cnn.py
@@ -0,0 +1,9 @@
+import fear_and_greed
+
+fear_and_greed.get()
+
+FearGreedIndex(
+    value=58,
+    description='Greed',
+    last_update=datetime.datetime(2022, 3, 18, 14, 11, tzinfo= "DstTzInfo 'US/Eastern' LMT-1 day, 19:04:00 STD")
+    )
