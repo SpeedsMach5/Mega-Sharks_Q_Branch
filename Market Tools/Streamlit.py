55b3c17ec21feeb46a44487305ec1eb6faaa3d86
Jacob Rougeau <oneor11@hotmail.com>
2022-01-08 19:17:03 -0600

    Refactor UI to emphasize configuration
    Refactored the UI by bringing all of the configuration options into the
    left panel.  Renamed some variables and functions to increase clarity.

--- Market Tools/Streamlit.py
+++ Market Tools/Streamlit.py
@@ -7,70 +7,119 @@ from plotly import graph_objects as go
 import pandas as pd
 import os
 
+#CONFIGURATION
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
+TICKER_LIST_PATH = 'data/constituents_symbols.txt'
+STRATEGY_LIST_PATH = 'data/strategies.txt'
 
-# ticker function
-def read_tickers():
-    with open("data/constituents_symbols.txt", "r") as file:
+status = st.text("")
+
+# UTILITY FUNCTIONS
+def get_listbox_data(file_path):
+    """Retrieves a list of data from a carriage return-delimited file
+
+    Opens the file specified by the file path and returns a list of its contents.
+
+    Parameters
+    ----------
+    file_path: str, required
+        The path of the file containing the list of items
+    
+    """
+
+    with open(file_path, "r") as file:
         content = file.read()
         content = content.split("\n")
         return content
 
-# Number of days to calculate
+# DATA FUNCTIONS
+def load_data_for_ticker(tickers, start_date, end_date):
+    """Retrieves pricing historical pricing data for a list of tickers
 
-stocks = (read_tickers())
-selected_stocks = st.sidebar.selectbox("Hunting the Markets", stocks)
+    Utilizes yfinance to return pricing data between a start and end date.
 
-n_day = st.slider("Days of predicitons", 1,10)
-period = n_day*10
-
-
-
-@st.cache
-
-def load_data(ticker):
-    data = yf.download(ticker, START, TODAY)
+    Parameters
+    ----------
+    tickers: str, required
+        A list of ticker symbols
+    
+    start_date: str, required
+        Download start date string (YYYY-MM-DD) or _datetime
+    
+    end_date: str, required
+        Download end date string (YYYY-MM-DD) or _datetime.
+    
+    """
+    data = yf.download(tickers, start_date, end_date)
     data.reset_index(inplace = True)
     return data
 
-data_load_state = st.text("Raw Hunt Data")
-data = load_data(selected_stocks)
-data_load_state.text("This is for entertainment purposes only and not a solicitation to buy or sell stocks.")
-
-st.subheader("Ticker Data for the past five closes")
-st.write(data.tail())
-
-#plot
-
-def plot_raw_data():
-    fig = go.Figure()
-    #fig.add_trace(go.Scatter(x=data["Date"], y=data["Open"], name="stock_open"))
-    fig.add_trace(go.Scatter(x=data["Date"], y=data["Close"], name="stock_close"))
-    fig.layout.update(title_text="Closes", xaxis_rangeslider_visible=True)
-    st.plotly_chart(fig,use_container_width=True)
-
-plot_raw_data()
-
-# Forecasting
-df_train = data[["Date", "Close"]]
-df_train = df_train.rename(columns={"Date":"ds", "Close": "y"})
-
-m = Prophet()
-m.fit(df_train)
-future = m.make_future_dataframe(periods=period)
-forecast = m.predict(future)
-
-st.subheader("Probable outcome, not an actual crystal ball")
-st.write(forecast.tail())
-
-st.write("Forecast Data")
-fig1 = plot_plotly(m, forecast)
-st.plotly_chart(fig1)
-
-st.write("Forecast Data")
-fig2 = m.plot_components(forecast)
-st.write(fig2)
+def run_analysis():
+    display_parameter_section()
+    data = display_ticker_data_section(ticker_selectbox, START, TODAY)
+    #display_sentiments()
+    display_forecasting_section(data)
 
 
+def display_parameter_section():
+    """Displays a summary of the user-selected parameters
+    
+    """
+    st.subheader("Parameters")
+    st.write('Summary of parameter selections:')
+    st.markdown('- Tickers: ' + ticker_selectbox)
+    st.markdown('- Strategies: ' + str(strategy_listbox))
+    st.markdown('- Prediction days: ' + str(n_day))
 
+def display_ticker_data_section(tickers, start_date, end_date):
+    st.subheader("Ticker Data: Last 5 Closes")
+    status = st.info("Loading...")
+    data = load_data_for_ticker(ticker_selectbox, start_date, end_date)
+    status.empty()
+    st.write(data.tail())
+    return data
+
+def display_forecasting_section(data):
+    st.subheader("Forecasting: Probable outcome, not an actual crystal ball")
+    status = st.info("Loading")
+    # Forecasting
+    df_train = data[["Date", "Close"]]
+    df_train = df_train.rename(columns={"Date":"ds", "Close": "y"})
+
+    m = Prophet()
+    m.fit(df_train)
+    future = m.make_future_dataframe(periods=period, freq='D')
+    forecast = m.predict(future)
+
+    st.write(forecast.tail())
+
+    #st.write("Forecast Data")
+    fig1 = plot_plotly(m, forecast)
+    st.plotly_chart(fig1)
+
+    #st.write("Forecast Data")
+    fig2 = m.plot_components(forecast)
+    st.write(fig2)
+    status.empty()
+
+
+# Linear Sidebar UI
+st.sidebar.subheader('Analysis Parameters')
+
+status.text = "Loading ticker list"
+ticker_list = get_listbox_data(TICKER_LIST_PATH)
+ticker_selectbox = st.sidebar.selectbox("1. Choose a ticker", ticker_list)
+st.sidebar.markdown('____')
+
+strategy_list = get_listbox_data(STRATEGY_LIST_PATH)
+strategy_listbox = st.sidebar.multiselect("2. Choose one or more trading strategies", strategy_list)
+st.sidebar.markdown('____')
+
+n_day = st.sidebar.slider("3. Choose number of prediction days", 1,10)
+period = n_day*10
+st.sidebar.markdown('____')
+
+st.sidebar.button('4. Run Analysis',help='Click to run analysis.', on_click=run_analysis)
+
+# Main area
