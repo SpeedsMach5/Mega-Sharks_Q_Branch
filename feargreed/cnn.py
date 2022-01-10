cae52a9f0d13a1bd85815f9d17e15a88b7845cc4
EGeorge2021r <egeorge2013khs@gmail.com>
2022-01-09 19:14:46 -0600

    Mega-Shark_EG_Branch

--- feargreed/cnn.py
+++
@@ -1,9 +0,0 @@
-import fear_and_greed #as fear_and_greed
-import datetime as dt
-FearGreed.get()
-
-FearGreedIndex(
-    value=58,
-    description='Greed',
-    last_update=datetime.datetime(2022, 3, 18, 14, 11, tzinfo= "DstTzInfo 'US/Eastern' LMT-1 day, 19:04:00 STD")
-    )
